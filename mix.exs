8fc8dd7cf56220ccb4b7f52eb14ba07d0988ccf2
Patrick <patrick@Macintosh.local>
2020-08-10 18:21:46 +0100

    +hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Furlong.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/patrick7777776/Furlong"
     ]
   end
 
@@ -16,6 +19,20 @@ defmodule Furlong.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Furlong: An Elixir port of the Kiwi Solver / Cassowary incremental constraint solving toolkit."
+  end
+
+  defp package() do
+    [
+      name: "furlong",
+      licenses: ["Apache 2"],
+      links: %{"GitHub" => "https://github.com/patrick7777776/Furlong"}
+    ]
   end
 end
